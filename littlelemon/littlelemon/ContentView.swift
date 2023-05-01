cd48f42f878126bb75cf1f1c52afe50a11cab04b
Khalid Kamil <khalidkamil28@gmail.com>
2023-05-01 19:34:05 +0100

    Project setup

---
+++ littlelemon/littlelemon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  littlelemon
+//
+//  Created by Khalid Kamil on 01/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ littlelemon/littlelemon/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  littlelemon
+//
+//  Created by Khalid Kamil on 01/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
