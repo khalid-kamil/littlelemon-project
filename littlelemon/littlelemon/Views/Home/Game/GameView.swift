74b51a50cf789e99a5624666bb6cfe7c71888be4
Khalid Kamil <khalidkamil28@gmail.com>
2023-06-10 13:15:04 +0100

    Add flagTapped and askQuestion functions

--- littlelemon/littlelemon/Views/Home/Game/GameView.swift
+++ littlelemon/littlelemon/Views/Home/Game/GameView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct GameView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
             Color.blue
                 .ignoresSafeArea()
+            
             VStack(spacing: 30) {
                 VStack {
                     Text("Rewards")
@@ -24,19 +28,41 @@ struct GameView: View {
                     Text("Tap the flag of")
                     Text(countries[correctAnswer])
                 }
+                .foregroundColor(.white)
                 
                 ForEach(0..<3) { number in
                     Button {
-                        // flag was tapped
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
                     }
-                    
                 }
             }
+            
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
         }
     }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
 }
 
 struct GameView_Previews: PreviewProvider {
