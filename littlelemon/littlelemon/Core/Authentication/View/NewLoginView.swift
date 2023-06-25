cf2d82e4db97b40c06fb48f4c3400ceed3d3d0bb
Khalid Kamil <khalidkamil28@gmail.com>
2023-06-25 20:11:53 +0100

    Embedded login and sign up forms in scrollview to prevent inconsistent UI changes when keyboard appears

--- littlelemon/littlelemon/Core/Authentication/View/NewLoginView.swift
+++ littlelemon/littlelemon/Core/Authentication/View/NewLoginView.swift
@@ -17,71 +17,70 @@ struct NewLoginView: View {
                     .padding(.vertical, 32)
                 
                 // form fields
-                VStack(spacing: 24) {
-                    InputView(text: $email,
-                              title: "Email Address",
-                              placeholder: "Enter your email address")
-                    .autocapitalization(.none)
+                ScrollView {
+                    VStack(spacing: 24) {
+                        InputView(text: $email,
+                                  title: "Email Address",
+                                  placeholder: "Enter your email address")
+                        .autocapitalization(.none)
+                        
+                        InputView(text: $password,
+                                  title: "Password",
+                                  placeholder: "Enter your password",
+                                  isSecureField: true)
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 12)
                     
-                    InputView(text: $password,
-                              title: "Password",
-                              placeholder: "Enter your password",
-                              isSecureField: true)
-                }
-                .padding(.horizontal)
-                .padding(.top, 12)
-                
-                // forgot password?
-                HStack {
-                    Spacer()
-                    Button {
-                        print("Resetting user password...")
-                    } label: {
-                        Text("Forgot Password?")
-                            .font(.footnote)
-                            .fontWeight(.semibold)
-                            .foregroundColor(Color(.systemBlue))
-                            .padding([.top, .trailing], 16)
-                    }
-                }
-                
-                // sign in button
-                Button {
-                    Task {
-                        try await authViewModel.signIn(withEmail: email, password: password)
-                    }
-                } label: {
+                    // forgot password?
                     HStack {
-                        Text("SIGN IN")
-                            .fontWeight(.semibold)
-                        Image(systemName: "arrow.right")
+                        Spacer()
+                        Button {
+                            print("Resetting user password...")
+                        } label: {
+                            Text("Forgot Password?")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color(.systemBlue))
+                                .padding([.top, .trailing], 16)
+                        }
                     }
-                    .foregroundColor(.white)
-                    .frame(width: UIScreen.main.bounds.width-32, height: 48)
-                }
-                .background(Color(.systemBlue))
-                .cornerRadius(12)
-                .padding(.top, 16)
-                .disabled(!formIsValid)
-                .opacity(formIsValid ? 1 : 0.3)
-                
-                Spacer()
-                
-                // sign up button
-                NavigationLink {
-                    RegistrationView()
-                        .navigationBarBackButtonHidden()
-                } label: {
-                    HStack(spacing: 0) {
-                        Text("Don't have an account? ")
-                        Text("Sign up")
-                            .fontWeight(.semibold)
-                            .foregroundColor(Color(.systemBlue))
+                    
+                    // sign in button
+                    Button {
+                        Task {
+                            try await authViewModel.signIn(withEmail: email, password: password)
+                        }
+                    } label: {
+                        HStack {
+                            Text("SIGN IN")
+                                .fontWeight(.semibold)
+                            Image(systemName: "arrow.right")
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: UIScreen.main.bounds.width-32, height: 48)
                     }
-                    .font(.system(size: 14))
-                    .padding(.bottom)
+                    .background(Color(.systemBlue))
+                    .cornerRadius(12)
+                    .padding(.top, 16)
+                    .disabled(!formIsValid)
+                    .opacity(formIsValid ? 1 : 0.3)
+                    
+                    // sign up button
+                    NavigationLink {
+                        RegistrationView()
+                            .navigationBarBackButtonHidden()
+                    } label: {
+                        HStack(spacing: 4) {
+                            Text("Don't have an account?")
+                            Text("Sign up")
+                                .fontWeight(.semibold)
+                                .foregroundColor(Color(.systemBlue))
+                        }
+                        .font(.system(size: 14))
+                        .padding(.vertical)
+                    }
                 }
-                
             }
         }
     }
--- littlelemon/littlelemon/Core/Authentication/View/RegistrationView.swift
+++ littlelemon/littlelemon/Core/Authentication/View/RegistrationView.swift
@@ -19,85 +19,88 @@ struct RegistrationView: View {
                 .padding(.vertical, 32)
             
             // form fields
-            VStack(spacing: 24) {
-                InputView(text: $email,
-                          title: "Email Address",
-                          placeholder: "Enter your email address")
-                .autocapitalization(.none)
-                
-                InputView(text: $fullname,
-                          title: "Full Name",
-                          placeholder: "Enter your full name")
-                .autocapitalization(.none)
-                
-                InputView(text: $password,
-                          title: "Password",
-                          placeholder: "Enter your password",
-                          isSecureField: true)
-                
-                ZStack(alignment: .bottomTrailing) {
-                    InputView(text: $confirmPassword,
-                              title: "Confirm Password",
-                              placeholder: "Re-enter your password",
+            ScrollView {
+                VStack(spacing: 24) {
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeholder: "Enter your email address")
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                    
+                    InputView(text: $fullname,
+                              title: "Full Name",
+                              placeholder: "Enter your full name")
+                    .autocapitalization(.none)
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
                               isSecureField: true)
                     
-                    if !password.isEmpty && !confirmPassword.isEmpty {
-                        if password == confirmPassword {
-                            Image(systemName: "checkmark.circle.fill")
-                                .foregroundColor(.green)
-                                .imageScale(.large)
-                                .fontWeight(.bold)
-                                .padding(.bottom, 8)
-                        } else {
-                            Image(systemName: "xmark.circle.fill")
-                                .foregroundColor(.red)
-                                .imageScale(.large)
-                                .fontWeight(.bold)
-                                .padding(.bottom, 8)
+                    ZStack(alignment: .bottomTrailing) {
+                        InputView(text: $confirmPassword,
+                                  title: "Confirm Password",
+                                  placeholder: "Re-enter your password",
+                                  isSecureField: true)
+                        
+                        if !password.isEmpty && !confirmPassword.isEmpty {
+                            if password == confirmPassword {
+                                Image(systemName: "checkmark.circle.fill")
+                                    .foregroundColor(.green)
+                                    .imageScale(.large)
+                                    .fontWeight(.bold)
+                                    .padding(.bottom, 8)
+                            } else {
+                                Image(systemName: "xmark.circle.fill")
+                                    .foregroundColor(.red)
+                                    .imageScale(.large)
+                                    .fontWeight(.bold)
+                                    .padding(.bottom, 8)
+                            }
                         }
                     }
+                    
                 }
+                .padding(.horizontal)
+                .padding(.top, 12)
                 
-            }
-            .padding(.horizontal)
-            .padding(.top, 12)
-            
-            
-            // sign up button
-            Button {
-                Task {
-                    try await authViewModel.createUser(withEmail: email, password: password, fullname: fullname)
+                
+                // sign up button
+                Button {
+                    Task {
+                        try await authViewModel.createUser(withEmail: email, password: password, fullname: fullname)
+                    }
+                } label: {
+                    HStack {
+                        Text("SIGN UP")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width-32, height: 48)
                 }
-            } label: {
-                HStack {
-                    Text("SIGN UP")
-                        .fontWeight(.semibold)
-                    Image(systemName: "arrow.right")
+                .background(Color(.systemBlue))
+                .cornerRadius(12)
+                .padding(.top, 16)
+                .disabled(!formIsValid)
+                .opacity(formIsValid ? 1 : 0.3)
+                
+                
+                
+                // sign in button
+                Button {
+                    dismiss()
+                } label: {
+                    HStack(spacing: 4) {
+                        Text("Already have an account? ")
+                        Text("Sign in")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color(.systemBlue))
+                    }
+                    .font(.system(size: 14))
+                    .padding(.vertical)
                 }
-                .foregroundColor(.white)
-                .frame(width: UIScreen.main.bounds.width-32, height: 48)
             }
-            .background(Color(.systemBlue))
-            .cornerRadius(12)
-            .padding(.top, 16)
-            .disabled(!formIsValid)
-            .opacity(formIsValid ? 1 : 0.3)
-            
-            Spacer()
-            
-            // sign in button
-            Button {
-                dismiss()
-            } label: {
-                HStack(spacing: 0) {
-                    Text("Already have an account? ")
-                    Text("Sign in")
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color(.systemBlue))
-                }
-                .font(.system(size: 14))
-            }
-            
         }
     }
 }
